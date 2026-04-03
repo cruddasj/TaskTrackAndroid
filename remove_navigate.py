be5b968fc33ddf8f8460f9d3208cfab878bca9bf
James Cruddas <james.cruddas@informed.com>
2026-04-03 09:54:27 +0100

    chore: remove unused local helper artifacts

--- remove_navigate.py
+++
@@ -1,7 +0,0 @@
-with open("src/screens/TodaysTasksScreen.tsx", "r") as f:
-    content = f.read()
-
-content = content.replace("import { useNavigate } from 'react-router-dom';\n", "")
-
-with open("src/screens/TodaysTasksScreen.tsx", "w") as f:
-    f.write(content)
