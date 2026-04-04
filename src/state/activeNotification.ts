b18a4196fdc8478e6a0433c40e757c3b78e15cef
James Cruddas <james.cruddas@informed.com>
2026-04-04 14:26:41 +0100

    Fix Android active timer notification sync behavior

--- src/state/activeNotification.ts
+++
@@ -1,4 +0,0 @@
-export const getActiveNotificationRemainingSeconds = (remainingSeconds: number, isAppActive: boolean): number => {
-  if (isAppActive) return remainingSeconds;
-  return Math.ceil(remainingSeconds / 60) * 60;
-};
